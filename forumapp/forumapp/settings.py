6bccb0ea53709967ba91269b3ffa68242e29ffb5
Sarthak Agrawal <sarthavader@gmail.com>
2020-11-02 21:43:15 -0600

    created local settings

--- forumapp/forumapp/settings.py
+++ forumapp/forumapp/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+from .local_settings import *
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gfhbqhtteeld@ztha*sw2avtg(oc)p6k&qeu)8_#+pq0)h1pl6'
+
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
