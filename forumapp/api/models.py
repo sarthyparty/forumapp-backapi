c6478632693cd84a29b41767402691eaf7a78462
Sarthak Agrawal <sarthavader@gmail.com>
2020-11-20 13:56:12 -0600

    setup new fields

--- forumapp/api/models.py
+++ forumapp/api/models.py
@@ -1,19 +1,19 @@
 from django.db import models
-from django.utils import timezone
 
 
 class Question(models.Model):
     content = models.CharField(max_length=10000)
-    time_asked = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
     email = models.EmailField(default="person@example.com")
     has_answer = models.BooleanField(default=False)
 
     def __str__(self):
         return self.content
 
+
 class Answer(models.Model):
     content = models.CharField(max_length=10000)
-    time_asked = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
     email = models.EmailField()
     question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name="answers")
 
--- forumapp/api/serializers.py
+++ forumapp/api/serializers.py
@@ -7,7 +7,7 @@ class QuestionSerializer(ModelSerializer):
     answers = PrimaryKeyRelatedField(many=True, read_only=True)
     class Meta:
         model = Question
-        fields = ('pk', 'content', 'time_asked', 'email', 'has_answer', 'answers')
+        fields = ('pk', 'content', 'email', 'created_at', 'has_answer', 'answers')
         extra_kwargs = {
             'id': {'read_only': True}
         }
@@ -15,7 +15,7 @@ class QuestionSerializer(ModelSerializer):
 class AnswerSerializer(ModelSerializer):
     class Meta:
         model = Answer
-        fields = ('pk', 'content', 'time_asked', 'email', 'question')
+        fields = ('pk', 'content', 'created_at', 'email', 'question')
         extra_kwargs = {
             'id': {'read_only': True}
         }
